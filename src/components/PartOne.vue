c73965f47a20255304cfe713acce67a795818919
wangyuming <wangyuming@etiantian.com>
2018-12-23 15:47:51 +0800

    上传代码

---
+++ src/components/PartOne.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="part-one">
+    <img class="part-one-image" :src="headImage" alt="头像">
+    <p>姓&emsp;&emsp;名：</p>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "partOne",
+  data() {
+    return {
+      headImage: require('../assets/img/head_image.png')
+    }
+  }
+};
+</script>
+
+<style scoped>
+a {
+  color: deepskyblue;
+}
+a:hover {
+  color: rgb(118, 190, 248);
+}
+p {
+  line-height: 30px;
+}
+.part-one {
+  width: 100%;
+  height: 500px;
+  border: 40px solid transparent;
+  border-image: url("~@/./assets/img/border_image.png") 30 30 stretch;
+  background: #18202d;
+  padding-left: 10px;
+}
+.part-one-image {
+  width: 150px;
+  height: 150px;
+}
+</style>
